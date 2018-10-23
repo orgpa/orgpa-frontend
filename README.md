c4be15c6a295f0269a5e06ef1e701b73171dfbda
Florent Poinsard <florent.poinsard@epitech.eu>
2018-10-23 23:35:52 +0200

    [ADDED] - angular repository

--- README.md
+++
@@ -1,2 +0,0 @@
-# orgpa-frontend
-Frontend of orgpa
---
+++ orgpa/src/app/mock.heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{id: 11, name: 'Mr. Nice'},
+	{id: 12, name: 'Narco'},
+	{id: 13, name: 'Bombasto'},
+	{id: 14, name: 'Celest'},
+	{id: 15, name: 'Truffade'},
+	{id: 16, name: 'Tartiflette'},
+	{id: 17, name: 'Pokemon'},
+	{id: 18, name: 'Toto'},
+	{id: 19, name: 'Jean Michel'},
+	{id: 20, name: 'Titi'}
+];
